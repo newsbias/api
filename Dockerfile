bf3d17ca080f47ede17704123d67779f8ed3a4d1
Nathan Moos <moosingin3space@gmail.com>
2018-02-07 19:40:37 -0500

    added docker-compose with postgresql container

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3
+ENV PYTHONUNBUFFERED 1
+ENV DOCKERIZE_VERSION v0.6.0
+
+RUN wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+    && tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+    && rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
+
+RUN pip install pipenv
+
+RUN mkdir /app
+WORKDIR /app
+ADD Pipfile /app/
+ADD Pipfile.lock /app/
+
+RUN pipenv install
+ADD . /app
+ENTRYPOINT [ "/usr/local/bin/pipenv", "run" ]
--- api/settings.py
+++ api/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', 'postgres'),
+        'USER': os.environ.get('DATABASE_USER', 'postgres'),
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),
+        'PORT': int(os.environ.get('DATABASE_PORT', '5432')),
+        'PASSWORD': os.environ.get('DATABASE_PASS', ''),
     }
 }
 
